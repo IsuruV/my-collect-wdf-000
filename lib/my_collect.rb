fe1566978478ed88977448288f990932cfde6049
Isuru Vidanapathirana <isuru260@gmail.com>
2016-08-08 16:09:10 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect (array)
+i = 0
+caps = []
+if block_given?
+while (i < array.length)
+  caps << yield(array[i])
+  i+=1
+end
+caps
+end
+"This block should not run!"
+caps
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
 
+my_collect(collection) do |lang|
+  puts lang.upcase
+end
